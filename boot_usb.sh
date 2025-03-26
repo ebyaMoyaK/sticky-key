d0fa412e973da82c19ddabc3ebe778381a62dbb6
Yanis Stentzel <114388444+ebyaMoyaK@users.noreply.github.com>
2025-03-26 01:31:16 +0100

    Update boot_usb.sh
    simplified lol

--- boot_usb.sh
+++ boot_usb.sh
@@ -17,43 +17,27 @@ ______      # ______      # __          # __          # ______      # __  __    
 #Ce script permet de créer une clé usb bootable
 
 # Récupération des noms des périphériques USB
-USB_DEVICES=$(lsblk -o NAME,SIZE,TYPE | grep 'disk' | awk '{print $1}')
-NUM_USB_DEVICES=$(echo "$USB_DEVICES" | wc -l)
+#!/bin/bash
 
-# Si un seul périphérique différent de /dev/sda est détécté, il est automatiquement choisi
-if [[ $NUM_USB_DEVICES -eq 1 ]] && [[ "$USB_DEVICES" != "sda" ]]
-then
-    USB_DEVICE="$USB_DEVICES"
-    echo "Périphérique USB détecté: ${USB_DEVICE}"
+# Détection du périphérique USB
+USB_DEVICES=( $(lsblk -nd -o NAME | grep -v '^sda$') )
+
+if [[ ${#USB_DEVICES[@]} -eq 1 ]]; then
+    USB_DEVICE=${USB_DEVICES[0]}
+    echo "Périphérique USB détecté: /dev/$USB_DEVICE"
 else
-    # Si plusieurs périphériques USB sont détectés, demander à l'utilisateur de choisir le périphérique
-    echo "Les périphériques USB suivants ont été détectés:"
-    lsblk -o NAME,SIZE,TYPE | grep 'disk'
-    echo
-
-    
-    echo "Veuillez choisir le périphérique USB:"
-    select USB_DEVICE in $USB_DEVICES
-    do
-        if [[ "$USB_DEVICE" != "sda" ]]
-        then
-            echo "Vous avez choisi le périphérique USB $USB_DEVICE."
-            break
-        else
-            echo "Le périphérique /dev/sda n'est pas pris en charge. Veuillez choisir un autre périphérique."
-        fi
+    echo "Sélectionnez un périphérique USB:"
+    select USB_DEVICE in "${USB_DEVICES[@]}"; do
+        [[ -n "$USB_DEVICE" ]] && break
     done
 fi
 
-
-# Demander confirmation avant d'écraser les données sur le périphérique USB
-read -p "Attention: toutes les données sur ${USB_DEVICE} seront écrasées. Continuer? (y/n) " -n 1 -r
+# Confirmation avant l'écriture
+read -p "Toutes les données sur /dev/$USB_DEVICE seront écrasées. Continuer? (y/n) " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    # Copier le fichier sur le périphérique USB
-    echo "Copie du fichier en cours..."
-    dd if=$1 of=/dev/${USB_DEVICE} bs=4M status=progress && sync
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Copie en cours..."
+    dd if="$1" of="/dev/$USB_DEVICE" bs=4M status=progress && sync
     echo "Copie terminée."
 else
     echo "Opération annulée."
